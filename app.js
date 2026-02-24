a22952766b72524d5a617f5c79f756be21c547c8
KnockOut19882 <mpodolski1988@gmail.com>
2026-02-24 21:19:04 +0100

    add alert for hit and miss

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let location1 = 3;
-let location2 = 4;
-let location3 = 5;
+const location1 = 3;
+const location2 = 4;
+const location3 = 5;
 
 let guess;
 let hits = 0;
@@ -9,13 +9,17 @@ let guesses = 0;
 let isSunk = false;
 
 while (isSunk == false) {
-  // guess = prompt('Ready, aim, fire! (Enter a number from 0-6:');
-  if (guess < 0 || guess > 6) {
+  let guess = Number(prompt('Ready, aim, fire! (Enter a number from 0-6):'));
+  if (guess < 0 || guess > 6 || isNaN(guess)) {
     alert('Please enter a valid number!');
   } else {
     guesses = guesses + 1;
-    if (guess == locatiopn1 || guess == location2 || guess == location3)
+    if (guess == location1 || guess == location2 || guess == location3) {
       hits = hits + 1;
+      alert('HIT!');
+    } else {
+      alert('MISS');
+    }
     if (hits == 3) {
       isSunk = true;
       alert('You sunk my battleship!');
